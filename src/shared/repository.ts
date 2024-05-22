446b756cc0d40c661921e84797b6fb4c3d277c6e
Juan Pastorino <juanjosepastorino@gmail.com>
2024-05-21 21:36:02 -0300

    agregacion delete

--- src/shared/repository.ts
+++ src/shared/repository.ts
@@ -3,5 +3,5 @@ export interface Repository<T> {
   findOne(item: {codigo:string}): T | undefined
   add(item: T): T | undefined
   update(item: T): T | undefined
-  //Agregar la declaración del método "delete"
+  delete(item: {id:string}): T  | undefined
 }
