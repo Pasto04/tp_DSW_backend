4eaf8ca7b9489ebc1c0c1287f7d4288a5e8d10d1
Juan Pastorino <juanjosepastorino@gmail.com>
2024-06-16 20:32:48 -0300

    clase cliente

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { tipoIngredienteRouter } from './tipo-ingrediente/tipo-ingrediente.routes.js';
 import { ingredienteRouter } from './ingrediente/ingrediente.routes.js';
+import { clienteRouter } from './cliente/cliente.routes.js';
 const port = 3000;
 const app = express();
 app.use(express.json());
 app.use('/api/tiposIngrediente', tipoIngredienteRouter);
 app.use('/api/ingredientes', ingredienteRouter);
+app.use('/api/cliente', clienteRouter);
 app.use((req, res) => {
     return res.status(404).send({ message: 'Recurso no encontrado' });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { tipoIngredienteRouter } from './tipo-ingrediente/tipo-ingrediente.routes.js'
 import { ingredienteRouter } from './ingrediente/ingrediente.routes.js'
+import { clienteRouter } from './cliente/cliente.routes.js'
 
 const port = 3000
 const app = express()
@@ -9,6 +10,8 @@ app.use(express.json())
 app.use('/api/tiposIngrediente', tipoIngredienteRouter)
 
 app.use('/api/ingredientes', ingredienteRouter)
+
+app.use('/api/cliente',clienteRouter)
 
 app.use((req, res) => {
   return res.status(404).send({message: 'Recurso no encontrado'})
