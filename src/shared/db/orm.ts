85706121f3c06111d0f6a285e4f21207cba0e4c7
Mauro F. Giraudo <mfgiraudo456@gmail.com>
2024-11-07 09:34:21 -0300

    Idea: Modificación de la funcionalidad: "Agregar Plato/Bebida al pedido",
    En un principio, la idea era incorporar un conjunto de platos/bebidas al pedido para que quedaran pendientes de entrega a la mesa. El problema es que estos se enviaban dentro del arreglo de platos/bebidas del pedido, respectivamente.
    Ahora, en lugar de hacer esto, buscaremos hacer uso del recurso "platoPedido/bebidaPedido" para agregar, uno a uno, el alimento que se desee al pedido.
    
    Se implementará esta funcionalidad en la rama "modificación_agregar_alimento_al_pedido". Una vez funcione, se incorporará a main.

--- src/shared/db/orm.ts
+++ src/shared/db/orm.ts
@@ -1,26 +1,26 @@
-import { MikroORM } from "@mikro-orm/core";
-import { SqlHighlighter } from "@mikro-orm/sql-highlighter";
+import { MikroORM } from '@mikro-orm/core';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
 
 export const orm = await MikroORM.init({
   entities: ['dist/**/*.entity.js'],
   entitiesTs: ['src/**/*entity.ts'],
   dbName: 'restaurante_dsw',
   type: 'mysql',
-  clientUrl: 'mysql://root:juan@localhost:3306/restaurante_dsw',
+  clientUrl: 'mysql://root:root@localhost:3306/restaurante_dsw',
   highlighter: new SqlHighlighter(),
   debug: true,
   schemaGenerator: {
     disableForeignKeys: true,
     createForeignKeyConstraints: true,
-    ignoreSchema: []
-  }
-})
+    ignoreSchema: [],
+  },
+});
 
-export const syncSchema = async() => {
+export const syncSchema = async () => {
   const generator = orm.getSchemaGenerator();
   /*
   await generator.dropSchema();
   await generator.createSchema();
   */
   await generator.updateSchema();
-}
+};
